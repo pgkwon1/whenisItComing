c52083c2cc11257ba775a58bf0132ab341c10501
HyukJin Kwon <pgkwon1@naver.com>
2018-04-10 02:14:19 +0000

    Fix typo

--- app/Alarm.php
+++ app/Alarm.php
@@ -7,17 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Alarm extends Model
 {
     //
-		protected $table = 'alarm';
-}
+    protected $table = 'alarm';
 
-public function up() {
-	Schema::create('alarm', function (Blueprint $table) {
-    $table->increments('id');
-    $table->string('token', 20);
-    $table->string('device', 30);
-    $table->Integer('arsId', 100);
-    $table->Integer('busRouteId', 250);
-    $table->timestamps();
-  });
-
+    public function up() 
+    {
+        Schema::create('alarm', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('token', 20);
+            $table->string('device', 30);
+            $table->Integer('arsId', 100);
+            $table->Integer('busRouteId', 250);
+            $table->timestamps();
+        });
+    }
 }
