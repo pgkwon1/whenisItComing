bda60503aab8d9787352cc1c9471a4eae03b0754
HyukJin Kwon <pgkwon1@naver.com>
2018-05-23 14:43:04 +0000

    Add api route

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/alarm','AlarmController@store');
+Route::post('/update','AlarmController@update');
+Route::get('/alarm/{id}','AlarmController@show');
