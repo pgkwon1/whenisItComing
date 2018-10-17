f395c3210cfe42e3a7ec0c68cd644e48f725aecf
pgkwon1@naver.com <pgkwon1@naver.com>
2018-10-17 23:51:04 +0900

    Add Model

---
+++ app/Http/Model/Alarm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Alarm extends Model
+{
+    //
+#    protected $fillable = array('id', 'device_id');
+
+    protected $table = 'alarm';
+}
