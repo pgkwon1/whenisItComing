caaed137e35304382213c1987c1fae98ad74b23a
pgkwon1@naver.com <pgkwon1@naver.com>
2018-10-17 23:48:56 +0900

    Function Development and Fix typo

--- app/Http/Controllers/Seoul/StationController.php
+++ app/Http/Controllers/Seoul/StationController.php
@@ -42,12 +42,12 @@ class StationController extends Controller
         $response = simplexml_load_string($response);
         foreach ($response->msgBody->itemList as $key => $val) {
             $list[] = (array)$val;
-        }        
+        }
 
         return $this->returnJson($list);
     }
 
-    public function getStationArrivalInfo($arsId = null, $bus = null)
+    public function getStationArrivalInfo($arsId = null)
     {
         $response = (string)$this->client
                     ->request('get', $this->end_point.'/getStationByUid?serviceKey='.$this->key.'&arsId='.$arsId)
@@ -56,20 +56,41 @@ class StationController extends Controller
         foreach ($response->msgBody->itemList as $key => $val) {
             $list[] = (array)$val;
         }
+        return $this->returnJson($list);
+    }
+
+    public function getArrivalInfo($arsId = null, $bus = null)
+    {
+        $response = (string)$this->client
+            ->request('get', $this->end_point.'/getStationByUid?serviceKey='.$this->key.'&arsId='.$arsId)
+            ->getBody();
+        $response = simplexml_load_string($response);
+        foreach ($response->msgBody->itemList as $key => $val) {
+            $list[] = (array)$val;
+        }
         $key = array_search($bus, array_column($list, 'rtNm'));
-        
-        return $this->returnKeyJson($key, $list);
+        $list[$key]['congetion'] = $this->getCongestion($list[$key]['busRouteId'], $list[$key]['sectOrd1']-1, $list[$key]['staOrd']);
+        return $this->returnKeyJson($key,$list);
     }
 
     public function getCongestion($bus_route_id = null, $start_ord = null, $end_ord = null)
     {
         $this->end_point = 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt';
         $response = (string) $this->client
-                     ->request('get', $this->end_point.'/?busRouteId='.$bus_route_id.'&startOrd='.$start_ord.'&endOrg='.$end_ord)
+                     ->request('GET', $this->end_point.'?serviceKey='.$this->key.'&busRouteId='.$bus_route_id.'&startOrd='.$start_ord.'&endOrd='.$end_ord)
                      ->getBody();
         $response = simplexml_load_string($response);
-        foreach ($response->msgBody->itemList as $key => $val) {
-            $list[] = (array)$val;
+        if (sizeof($response->msgBody->itemList)) {
+            foreach ($response->msgBody->itemList as $key => $val) {
+                $list[0] = (array)$val;
+            }
+            if (!$list[0]['congetion']) {
+                return "0";
+            } else {
+                return $list[0]['congetion'];
+            }
+        } else {
+            return false;
         }
     }
 }
